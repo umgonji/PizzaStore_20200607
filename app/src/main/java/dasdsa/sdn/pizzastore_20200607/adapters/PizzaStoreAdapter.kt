356aed3110a03c0600d4440215616464d03f5ea5
umgonji <umgonji@naver.com>
2020-06-07 15:08:45 +0900

    피자가게 이름까지만 실제로 띄어보기

--- app/src/main/java/dasdsa/sdn/pizzastore_20200607/adapters/PizzaStoreAdapter.kt
+++ app/src/main/java/dasdsa/sdn/pizzastore_20200607/adapters/PizzaStoreAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.TextView
 import dasdsa.sdn.pizzastore_20200607.R
 import dasdsa.sdn.pizzastore_20200607.datas.PizzaStore
@@ -25,12 +26,20 @@ class PizzaStoreAdapter(context: Context, resourceId: Int, list: List<PizzaStore
         }
         val row = tempRow!!
 
-//        val pizzastoreInfoTxt = row.findViewById<TextView>(R.id.pizzastoreNameTxt)
+        //그려진 XML에 실제 피자가게 데이터를 반영
+        // 데이터가 반영될 뷰들을 가져오는 작
 
-//        val data = mList.get(position)
+        // =오른쪽은 pizza_store_list_item.xml 인거다. 거기서 찾는것임. row 가 .xml 인것이지.
+        val logoImg = row.findViewById<ImageView>(R.id.logoImg)
+        val storeNameTxt = row.findViewById<TextView>(R.id.storeNameTxt)
 
+        //뷰에 뿌려질 데이터를 들고 있는 변수를 생성
+        //액티비티가 전달해준 목록인 mList에서 꺼내보자.
+        val data = mList.get(position)
 
-//        pizzastoreInfoTxt.text = "${data.name}"
+        //data안에 있는 가게이름을 => storeNameTxt의 글자로 설정
+
+        storeNameTxt.text = data.name
 
         return row
     }
