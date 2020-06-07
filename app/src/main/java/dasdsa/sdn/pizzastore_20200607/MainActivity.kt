b4c9653c35c759019f0bef03221c979c52cbe4b0
umgonji <umgonji@naver.com>
2020-06-07 14:53:40 +0900

    피자가게 정보 추가함

--- app/src/main/java/dasdsa/sdn/pizzastore_20200607/MainActivity.kt
+++ app/src/main/java/dasdsa/sdn/pizzastore_20200607/MainActivity.kt
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dasdsa.sdn.pizzastore_20200607.adapters.PizzaStoreAdapter
 import dasdsa.sdn.pizzastore_20200607.datas.PizzaStore
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
-    val pizzastore = ArrayList<PizzaStore>()
+    val pizzaStoreList = ArrayList<PizzaStore>()
 
     lateinit var pizzastoreAdapter:PizzaStoreAdapter
 
@@ -24,12 +25,22 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
-        pizzastore.add(PizzaStore("피자헛", "img_01", "01011112222" ))
-        pizzastore.add(PizzaStore("파파존스", "img_01", "020001234" ))
+        addStores()
 
-   //     pizzastoreAdapter = PizzaStoreAdapter(this, R.layout.pizza_store_list_item)
+    //    pizzaStoreListView.adapter
+    }
 
 
+
+
+    fun addStores() {
+        pizzaStoreList.add(PizzaStore("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", "1588-5588" ))
+        pizzaStoreList.add(PizzaStore("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", "1577-8080" ))
+        pizzaStoreList.add(PizzaStore("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", "1577-0077" ))
+        pizzaStoreList.add(PizzaStore("도미노피자", "https://pbs.twimg.com/profile_images/1098371010548555776/trCrCTDN_400x400.png", "1577-3082" ))
+
+
+        pizzastoreAdapter = PizzaStoreAdapter(this, R.layout.pizza_store_list_item, pizzaStoreList)
     }
 
 
