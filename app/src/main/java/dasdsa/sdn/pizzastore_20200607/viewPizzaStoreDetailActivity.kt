503f6958241a7b0b1b62edfacbc3e374705afbd9
umgonji <umgonji@naver.com>
2020-06-07 16:18:57 +0900

    serializable로 클릭 된 가게를 상세화면으로 통째로 전달

--- app/src/main/java/dasdsa/sdn/pizzastore_20200607/viewPizzaStoreDetailActivity.kt
+++ app/src/main/java/dasdsa/sdn/pizzastore_20200607/viewPizzaStoreDetailActivity.kt
@@ -2,8 +2,17 @@ package dasdsa.sdn.pizzastore_20200607
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import dasdsa.sdn.pizzastore_20200607.datas.PizzaStore
+import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.*
+import kotlinx.android.synthetic.main.activity_view_pizza_store_detail.logoImg
+import kotlinx.android.synthetic.main.pizza_store_list_item.*
 
 class viewPizzaStoreDetailActivity : BaseActivity() {
+
+    lateinit var  mPizzaStore : PizzaStore
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_pizza_store_detail)
@@ -15,9 +24,23 @@ class viewPizzaStoreDetailActivity : BaseActivity() {
 
     override fun setupevents() {
 
+        callBtn.setOnClickListener {
+            val pl = object : permissionl
+
+        }
+
     }
 
     override fun setValues() {
+        // 받을때 Serializable 형식으로 받아야 한다. 왜냐면 PizzaStore 가 Serializable 이니까.
+        // Serializable 형태로 첨부된 가게 정보를 받아서 => PizzaStore 형태로 변신시켜서 (Casting 시킨다)
+        // 멤버변수로 있는 피자가게 변수에 대입
+        mPizzaStore = intent.getSerializableExtra("store") as PizzaStore
+
+        //대입 된 피자 가게 변수의 데이터들을 이용해서 화면에 출력
+        Glide.with(mContext).load(mPizzaStore.logoUrl).into(logoImg)
+        storeNameTxt.text = mPizzaStore.name
+        phoneNumTxt.text = mPizzaStore.PhoneNum
 
     }
 
